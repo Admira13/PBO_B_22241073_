0f879b32a3c21cda3c85515b1212df8679a454ae
Admira13 <146043911+Admira13@users.noreply.github.com>
2023-10-31 11:27:09 +0800

    31/10/2023

---
+++ 01-PengenalanClassdanObjek/src/com/tutorial/Main.java
@@ -0,0 +1,19 @@
+package com.tutorial;
+class Mahasiswa {
+    String Nama;
+    String NIM;
+    String Prodi;
+public class Main {
+    ]
+    public static void main(String[] args) {
+        // instansiasi objek dari class Mahasiswa
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.nama = "Admira";
+        mhs1.NIM = "22241073";
+        mhs1.Prodi = "PTI";
+        System.out.printl ("Nama : " + mhs1.Nama);
+        System.out.printl ("Nama : " + mhs1.NIM);
+        System.out.printl ("Nama : " + mhs1.Prodi);
+        
+    }
+}
